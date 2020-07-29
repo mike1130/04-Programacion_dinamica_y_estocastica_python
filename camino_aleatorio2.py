ebbaace361b37267eeabbed0cc08e0d21d949ffe
Michel Caraballo <caraballomichel@gmail.com>
2020-07-29 07:41:50 -0500

    grafica del camino del borracho

---
+++ camino_aleatorio2.py
@@ -0,0 +1,72 @@
+
+from borracho import BorrachoTradicional
+from campo import Campo
+from coordenada import Coordenada
+from bokeh.plotting import figure, show
+
+def caminata(campo, borracho, pasos):
+    x_arreglo = []
+    y_arreglo = []
+
+    x_arreglo.append(campo.obtener_coordenada(borracho).x)
+    y_arreglo.append(campo.obtener_coordenada(borracho).y)
+
+    for _ in range(pasos):
+        campo.mover_borracho(borracho)
+        x_arreglo.append(campo.obtener_coordenada(borracho).x)
+        y_arreglo.append(campo.obtener_coordenada(borracho).y)
+
+    return x_arreglo, y_arreglo
+
+
+# def simular_caminata(pasos, numero_de_intentos, tipo_de_borracho):
+#     borracho = tipo_de_borracho(name='David')
+#     origen = Coordenada(0, 0)
+#     distancias = []
+
+#     for _ in range(numero_de_intentos): #_ por cada intento, no se usa variable
+#         campo = Campo()
+#         campo.anadir_borracho(borracho, origen)
+#         simulacion_caminata = caminata(campo, borracho, pasos)
+#         distancias.append(round(simulacion_caminata, 1))
+
+#     return distancias
+
+
+def graficar(x ,y):
+    grafica = figure(title='Camino de Borrachos', x_axis_label='pasos', y_axis_label='pasos')
+    grafica.line(x, y, legend='Caminata')
+
+    show(grafica)
+
+
+def main(distancias_de_caminata, inicio, tipo_de_borracho):
+    # distancias_media_por_caminata = []
+
+    # for pasos in distancias_de_caminata:
+    #     distancias = simular_caminata(pasos, numero_de_intentos, tipo_de_borracho)
+    #     distancia_media = round(sum(distancias) / len(distancias), 4)
+    #     distancia_maxima = max(distancias)
+    #     distancia_minima = min(distancias)
+    #     distancias_media_por_caminata.append(distancia_media)
+    #     print(f'{tipo_de_borracho.__name__} caminata aleatoria de {pasos} pasos')
+    #     print(f'Media = {distancia_media}')
+    #     print(f'Máxima = {distancia_maxima}')
+    #     print(f'Mínima = {distancia_minima}')
+
+    # graficar(distancias_de_caminata, distancias_media_por_caminata)
+
+    campo = Campo()
+    campo.anadir_borracho(tipo_de_borracho, inicio)
+
+    recorrido = caminata(campo,tipo_de_borracho, distancias_de_caminata)
+
+    graficar(recorrido[0], recorrido[1])
+
+
+if __name__ == '__main__':
+    distancias_de_caminata = 10000 #[10, 100, 1000, 10000]
+    #numero_de_intentos = 100
+    inicio = Coordenada(0, 0)
+
+    main(distancias_de_caminata, inicio, BorrachoTradicional(name='Michel'))
